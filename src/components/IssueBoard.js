e09da0d6595aeb23a95095a24fb37f9a39efdbed
Hussain Ali <ali.415code@gmail.com>
2021-02-01 14:42:40 -0800

    added issue add btn

--- src/components/IssueBoard.js
+++ src/components/IssueBoard.js
@@ -5,6 +5,7 @@ export default function IssueBoard(props) {
 
     return (
         <div>
+            <button className='addIssue-btn'>Add Issue</button>
             <button className='logout' onClick={props.logout}>Logout</button>
         </div>
     )
