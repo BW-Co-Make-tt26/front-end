b47f37542bf954f02962f64d95ea0f8ab9a26886
Hussain Ali <ali.415code@gmail.com>
2021-02-02 10:45:20 -0800

    Rendering issues matched with current user id

--- src/components/MyIssues.js
+++ src/components/MyIssues.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import {axiosWithAuth} from '../utils/axiosWithAuth'
+import IssueCard from './IssueCard'
 
-export default function MyIssues() {
+export default function MyIssues(props) {
+
+    const [myIssues, setMyIssues] = useState([])
+
+    const curUser = JSON.parse(localStorage.getItem('user')) // get user data
+    const curUserId = curUser.user_id                       // target user_id to filter through all issues
+
+
+    useEffect(() => {                                        // get data issues array
+        axiosWithAuth()
+        .get(`/api/issues/`)
+        .then(res => {
+            setMyIssues(res.data)
+        })
+    }, [])
+    console.log(myIssues)
+
+
+    const myIssuesArr = myIssues.filter(item => item.user_id === curUserId ? item : null )  // myIssues Array
+    console.log(myIssuesArr)
+    
     return (
-        <div>
-            
-        </div>
+        <>
+        <h2>My Issues</h2>
+            <div className='issues-container'>
+                {myIssuesArr.map((item, index) => {
+                    return <IssueCard key={index} issue={item}/>
+                })}
+            </div>
+        </>
     )
 }
